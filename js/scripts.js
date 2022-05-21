d7f1a06499790340ecc446ba67d15c1e99849da4
Voopsy <105043512+Voopsy@users.noreply.github.com>
2022-05-21 08:37:27 +0500

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+let names = [
+    'Чебоксар',
+    'Чечя',
+    'Чмоня',
+    'Саня',
+    'Илюха',
+    'Фуджин',
+    'Роберт',
+    'Корга',
+    'Петенька',
+]
+console.log(names);
+console.log(names.length)
+console.log(names[2]);
